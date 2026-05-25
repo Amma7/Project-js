f4a3c244ae21155a5fda1df95be7d78373ea129f
Ammarnet <115216900+Amma7@users.noreply.github.com>
2026-05-25 19:28:53 +0300

    Delete speak1.js

--- speak1.js
+++
@@ -1,9 +0,0 @@
-( function(window){
-   var yaahkeGerrter = {};
-   yaahkeGerrter.name = "Ahmed";
-   var greeting = "speaker with: ";
-   yaahkeGerrter.sayhi = function (){
-    console.log(greeting  + yaahkeGerrter.name );
-   }
-   window.yaahkeGerrter = yaahkeGerrter
-})(window);
